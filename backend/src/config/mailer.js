8f85e6de954b3c819d966ee4a9d3e4fc6e1bb0c6
kambabouesso-dot <kamba.bouesso@gmail.com>
2026-08-31 00:45:52 +0200

    Ajoute des delais SMTP au formulaire de contact

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -9,6 +9,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 20000,
 });
 
 module.exports = transporter;
